04da58f39b8a1d3f5f8a39f4f66019fcc9e26e74
MatiRosen <matias.rosenstein00@gmail.com>
2021-06-07 16:49:37 -0300

    CleanUp code

--- src/main/java/io/github/matirosen/chatbot/modules/CoreModule.java
+++ src/main/java/io/github/matirosen/chatbot/modules/CoreModule.java
@@ -3,6 +3,7 @@ package io.github.matirosen.chatbot.modules;
 import io.github.matirosen.chatbot.BotPlugin;
 import io.github.matirosen.chatbot.listeners.ChatListener;
 import io.github.matirosen.chatbot.managers.FileManager;
+import io.github.matirosen.chatbot.managers.MessageManager;
 import me.yushust.inject.Binder;
 import me.yushust.inject.Module;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -21,6 +22,7 @@ public class CoreModule implements Module {
         binder.bind(BotPlugin.class).toInstance(plugin);
 
         binder.bind(FileManager.class).singleton();
+        binder.bind(MessageManager.class).singleton();
         binder.bind(ChatListener.class).singleton();
     }
 }
